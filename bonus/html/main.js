57d4e805556dfe3dd95242758c15c35e4a660c3b
MarcoVacchi <mvacchi816@gmail.com>
2025-03-04 14:50:47 +0100

    create bonus

---
+++ bonus/html/main.js
@@ -0,0 +1,27 @@
+const elementPick = document.getElementById ("no-light");
+const elementButton = document.getElementById ("my-button");
+
+//console.log(elemenButton);
+//console.log(elementPick);
+
+elementButton.addEventListener("click", function (){
+   if (addEventListener) {elementPick.src =  "../img/yellow_lamp.png";
+    elementPick.alt = "lampadina accesa";
+    elementButton.innerText = "Spegni";
+
+   } else {
+    elementPick.src =  "../img/white_lamp.png";
+    elementButton.innerText = "Accendi";
+   }  
+});
+
+
+/*BONUS
+Facciamo accendi e spegni:
+- Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+- Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+- E così via...
+Per fare questo bonus potremmo aver bisogno di del metodo string.includes()
+Come detto a lezione, attenetevi al testo.
+Se volete sbizzarrirvi, fatelo in una cartella ulteriore (tipo bonus-1, bonus-2)
+Buon Lavoro e buon divertimento! :saluto_vulcaniano:*/
