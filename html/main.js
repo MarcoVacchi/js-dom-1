d4a30b8602ad92fd2ca859654448a6251b72f23a
MarcoVacchi <mvacchi816@gmail.com>
2025-03-04 14:26:46 +0100

    feat: exercise complete

--- html/main.js
+++ html/main.js
@@ -1,1 +1,11 @@
-console.log('ciao mondo');
+const elementPick = document.getElementById ("no-light");
+const elementButton = document.getElementById ("my-button");
+
+//console.log(elemenButton);
+//console.log(elementPick);
+
+elementButton.addEventListener("click", function (){
+    elementPick.src =  "../img/yellow_lamp.png";
+    elementPick.alt = "lampadina accesa";
+    console.log(elementPick); 
+});
